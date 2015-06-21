9883c36f1148d9f4fc1b5fd84c6794f84606c924
Ian G <yo.ian.g@gmail.com>
2015-06-21 16:52:29 -0400

    Framework support

---
+++ JMImageCacheFramework.h
@@ -0,0 +1,19 @@
+//
+//  JMImageCache.h
+//  JMImageCache
+//
+//  Created by Ian on 6/21/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JMImageCache.
+FOUNDATION_EXPORT double JMImageCacheVersionNumber;
+
+//! Project version string for JMImageCache.
+FOUNDATION_EXPORT const unsigned char JMImageCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JMImageCache/PublicHeader.h>
+
+
